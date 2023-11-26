4772ca7cac127b6056dce7bcdf0695963d7fde3e
Joni-Samanen <93249915+Joni-Samanen@users.noreply.github.com>
2023-11-25 21:13:47 -0500

    random fix mongo

--- Group 23 Final Project/G23App/app.js
+++ Group 23 Final Project/G23App/app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
-app.use("/tournament", TourneyRouter);
+app.use("/tourney", TourneyRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
