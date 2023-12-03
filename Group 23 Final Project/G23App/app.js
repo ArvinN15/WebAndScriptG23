279eaea1c210b623208d4cb4efd59f5bb81c8ecb
Joni-Samanen <93249915+Joni-Samanen@users.noreply.github.com>
2023-12-03 17:29:16 -0500

    added express-session and connect-flash as dependencies, and made the app use them

--- Group 23 Final Project/G23App/app.js
+++ Group 23 Final Project/G23App/app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 let mongoose = require("mongoose");
 let DB = require("./db");
+let flash = require("connect-flash")
+let session = require("express-session");
 
 mongoose.connect(DB.URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
@@ -24,11 +26,21 @@ var app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(flash());
+
+app.use(session({
+  secret: "your-secret-key",
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
@@ -38,6 +50,8 @@ app.use("/tourney", TourneyRouter);
 app.use(function (req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function (err, req, res, next) {
